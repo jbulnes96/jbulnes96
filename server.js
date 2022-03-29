263b0be2e1ed8e21bdcb7ed68c4095e0f9cec342
Jorge Bulnes <97678857+jbulnes96@users.noreply.github.com>
2022-03-29 19:09:14 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-
+const app = express();
 const PORT = process.env.PORT || 5000;
 app.set('port', (process.env.PORT || 5000));
-const app = express();           
+           
   
 
 
